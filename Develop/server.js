b8a05cf7eecef9b78e62196b5c4c767277b7461e
Jason Strouphauer <jdstroup10@gmail.com>
2020-05-06 22:24:52 -0500

    reversing changes made to mongodb code

--- Develop/server.js
+++ Develop/server.js
@@ -20,9 +20,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-mongoose.connect(MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 
 //HTML routes go here
