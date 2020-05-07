674329891da65c20216ec2ebfd4821e97a4a7101
Jason Strouphauer <jdstroup10@gmail.com>
2020-05-06 21:42:03 -0500

    Adding finished routes and deleted unused route folder

--- Develop/server.js
+++ Develop/server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
+var path = require("path");
 
-require("./routes/htmlRoutes")
-require("./routes/apiRoutes")
+const PORT = process.env.PORT || 3050;
+
+//require("./routes/htmlRoutes")
+//require("./routes/apiRoutes")
 
 const db = require("./models");
 
@@ -18,8 +20,64 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
+
+//HTML routes go here
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/exercise", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/exercise.html"));
+});
+
+app.get("/stats", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/stats.html"));
+});
+
+
+//API routes go here
+app.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+//Grabs info from the database and fills in the stats page 
+app.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+//creates new exercise
+app.post("/api/workouts", function (req, res) {
+  db.Workout.create({}).then(data =>
+    res.json(data)).catch(err => {
+      console.log(err)
+      res.json(err)
+    })
+});
+
+app.put("/api/workouts/:id", ({body, params}, res) => {
+  db.Workout.findByIdAndUpdate(
+      params.id,{
+          $push: {exercises: body} },
+          {new: true}
+  ).then (data => res.json(data))
+  .catch(err => {
+      console.log(err)
+      res.json(err)
+  })
+});
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-  });
+  console.log(`App running on port ${PORT}!`);
+});
